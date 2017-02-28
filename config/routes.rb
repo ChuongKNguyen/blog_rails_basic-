46f42c62a1378c7ee101c2449b48f6ef05e8849b
Chuong Nguyen <tinchuong@yahoo.fr>
2017-02-28 14:39:39 -0500

    log in with gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'home/index'
+  devise_for :users
+  resources :posts do
+  	resources :comments
+  end
+
+  root "posts#index"
+
   get '/about', to: 'pages#about'
-
-  resources :users
-  resources :posts do 
-  	resources :comments 
-  end 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
 end
