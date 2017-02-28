6fa7b625344af6801f399da0fa9b185ed40dcd3f
Chuong Nguyen <tinchuong@yahoo.fr>
2017-02-28 17:23:22 -0500

    log out, display user session, sign up with devise problem fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  
   resources :posts do
   	resources :comments
   end
