223e48fd83bc6144b3b3acc8edaa44e304831d12
Chuong Nguyen <tinchuong@yahoo.fr>
2017-02-27 22:07:04 -0500

    post added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   resources :users
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
