4b227cf8a8e649f0de9f7f933de6cadd8ae55cd5
Chuong Nguyen <tinchuong@yahoo.fr>
2017-02-28 12:24:16 -0500

    add comment in post.rb and routes for comments, controller comments created, comment.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   get 'users/index'
 
   get 'home/index'
+
   resources :users
-  resources :posts
+  resources :posts do 
+  	resources :comments 
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end
